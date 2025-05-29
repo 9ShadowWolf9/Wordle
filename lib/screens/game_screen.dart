293e9497ac894a49612cc9e57fdc831cc141c473
9ShadowWolf9 <grzegorzs0416@gmail.com>
2025-05-29 10:30:00 +0200

    Added Flushbar for wrong word

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -3,9 +3,23 @@ import 'package:provider/provider.dart';
 import 'package:wordle/components/letter_board_component.dart';
 import 'package:wordle/models/game_logic.dart';
 import 'package:wordle/widgets/keyboard_tile.dart';
+import 'package:another_flushbar/flushbar.dart';
 
 class GameScreen extends StatelessWidget {
   const GameScreen({super.key});
+
+  void _showInvalidWordNotification(BuildContext context) {
+    Flushbar(
+      message: 'Podane słowo nie istnieje',
+      title: "Błąd",
+      duration: const Duration(milliseconds: 900),
+      flushbarPosition: FlushbarPosition.TOP,
+      backgroundColor: Colors.redAccent,
+      margin: const EdgeInsets.all(8),
+      borderRadius: BorderRadius.circular(8),
+      icon: const Icon(Icons.error_outline, color: Colors.white),
+    ).show(context);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +27,11 @@ class GameScreen extends StatelessWidget {
 
     void handleKeyPress(String key) {
       if (key == 'ENTER') {
-        gameState.submitWord();
+        final isValid = gameState.submitWord();
+
+        if (!isValid) {
+          _showInvalidWordNotification(context);
+        }
       } else if (key == 'BACKSPACE') {
         gameState.removeLetter();
       } else {
