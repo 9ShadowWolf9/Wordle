5faf55e2870ddad9478008e59640141419c1c1a1
9ShadowWolf9 <grzegorzs0416@gmail.com>
2025-05-29 12:05:39 +0200

    Added third theme

--- lib/themes/theme_provider.dart
+++ lib/themes/theme_provider.dart
@@ -1,23 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:wordle/themes/dark_mode.dart';
 import 'package:wordle/themes/light_mode.dart';
+import 'package:wordle/themes/night_mode.dart';
+
+enum AppTheme { light, dark, night }
 
 class ThemeProvider extends ChangeNotifier {
-  ThemeData _themeData = lightMode;
+  AppTheme _currentTheme = AppTheme.light;
 
-  ThemeData get themeData => _themeData;
-  bool get isDarkMode => _themeData == darkMode;
-
-  set themeData(ThemeData themeData) {
-    _themeData = themeData;
-    notifyListeners();
+  ThemeData get themeData {
+    switch (_currentTheme) {
+      case AppTheme.dark:
+        return darkMode;
+      case AppTheme.night:
+        return nightMode;
+      case AppTheme.light:
+      default:
+        return lightMode;
+    }
   }
 
-  void toggleTheme() {
-    if (_themeData == lightMode) {
-      themeData = darkMode;
-    } else {
-      themeData = lightMode;
-    }
+  AppTheme get currentTheme => _currentTheme;
+
+  void setTheme(AppTheme theme) {
+    _currentTheme = theme;
+    notifyListeners();
   }
 }
